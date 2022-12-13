45f260c8376202dcd2b0097ca973b60a91c57ff6
GGVidal <gabrielvidalgondim@gmail.com>
2022-12-13 20:13:10 -0300

    feat: adding cache config in th eapp

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,33 @@
 
 import React from 'react';
 
-import {createClient, Provider as UrqlProvider} from 'urql';
+import {
+  createClient,
+  dedupExchange,
+  fetchExchange,
+  Provider as UrqlProvider,
+} from 'urql';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import {RootNavigator} from './screens/Root.navigator';
+import {cacheExchange} from '@urql/exchange-graphcache';
+import schema from './graphql/graphql.schema.json';
 
-const client = createClient({url: 'http://localhost:3000/graphql'});
+const client = createClient({
+  url: 'http://localhost:3000/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      schema: schema as any,
+      resolvers: {
+        Query: {
+          story: (_, args) => ({__typename: 'Story', id: args.id}),
+        },
+      },
+    }),
+    fetchExchange,
+  ],
+});
 
 const App = () => {
   return (
