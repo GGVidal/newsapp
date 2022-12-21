c36cd36044aae87362e56e82edfb693e278d90f8
GGVidal <gabrielvidalgondim@gmail.com>
2022-12-21 12:33:52 -0300

    feat: adding offline support

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,16 @@ import React from 'react';
 import {
   createClient,
   dedupExchange,
+  Exchange,
   fetchExchange,
   gql,
   Provider as UrqlProvider,
+  makeErrorResult,
 } from 'urql';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import {RootNavigator} from './screens/Root.navigator';
-import {cacheExchange} from '@urql/exchange-graphcache';
+import {offlineExchange} from '@urql/exchange-graphcache';
 import schema from './graphql/graphql.schema.json';
 import {
   AddBookmarkMutation,
@@ -29,12 +31,59 @@ import {
   RemoveBookmarkMutationVariables,
 } from './graphql/__generated__/operationTypes';
 import {BOOKMARKS_QUERY} from './queries/allBookmarks.graphql';
+import {useNetInfo} from '@react-native-community/netinfo';
+// import {AppOfflinePage} from './components/AppOffline';
+import {AppOfflineMessage} from './components/AppOfflineMessage';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {makeAsyncStorage} from '@urql/storage-rn';
+import NetInfo from '@react-native-community/netinfo';
+import {share, pipe, filter, map, merge} from 'wonka';
+
+let disconnect: any;
+
+const offlineMutationExchange: () => Exchange = () => {
+  let connected = true;
+
+  if (disconnect) {
+    disconnect();
+    disconnect = undefined;
+  }
+  disconnect = NetInfo.addEventListener(state => {
+    connected = !!state.isConnected;
+  });
+  return ({forward}) => {
+    return ops$ => {
+      const shared = pipe(ops$, share);
+
+      const offlineMutations = pipe(
+        shared,
+        filter(op => op.kind === 'mutation' && !connected),
+        map(op => makeErrorResult(op, new Error('You are offline!'))),
+      );
+
+      const restOps = pipe(
+        shared,
+        filter(
+          op => op.kind !== 'mutation' || (op.kind === 'mutation' && connected),
+        ),
+      );
+      return merge([forward(restOps), offlineMutations]);
+    };
+  };
+};
+
+const storage = makeAsyncStorage({
+  dataKey: 'my-app-data',
+  metadataKey: 'my-app-metadata',
+  maxAge: 5,
+});
 
 const client = createClient({
   url: 'http://localhost:3000/graphql',
   exchanges: [
     dedupExchange,
-    cacheExchange({
+    offlineExchange({
+      storage,
       schema: schema as any,
       resolvers: {
         Query: {
@@ -91,18 +140,27 @@ const client = createClient({
         },
       },
     }),
+    offlineMutationExchange(),
     fetchExchange,
   ],
 });
 
 const App = () => {
+  const {isConnected} = useNetInfo();
+
+  // if (!isConnected) {
+  //   return <AppOfflinePage />;
+  // }
   return (
-    <UrqlProvider value={client}>
-      <NavigationContainer>
-        <StatusBar hidden />
-        <RootNavigator />
-      </NavigationContainer>
-    </UrqlProvider>
+    <SafeAreaProvider>
+      <UrqlProvider value={client}>
+        <NavigationContainer>
+          <StatusBar hidden />
+          <RootNavigator />
+        </NavigationContainer>
+        {!isConnected && <AppOfflineMessage />}
+      </UrqlProvider>
+    </SafeAreaProvider>
   );
 };
 
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,8 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {
   ActivityIndicator,
+  Alert,
   Pressable,
   StyleSheet,
   Text,
@@ -44,7 +45,25 @@ export const Story: React.FC<StoryProps> = ({
     RemoveBookmarkMutationVariables
   >(REMOVE_BOOKMARK_MUTATION);
 
-  console.log('GG BOOKMARK ID', bookmarkId, isRemovingBookmark, cta)
+  const handleAddBookmark = useCallback(async () => {
+    const result = await addBookmark({storyId: id});
+    if (result.error && result.error.message.includes('You are offline!')) {
+      Alert.alert(
+        'You are offline',
+        'Please connect to the internet to add this story to your bookmarks',
+      );
+    }
+  }, [addBookmark, id]);
+
+  const handleRemoveBookmark = useCallback(async () => {
+    const result = await removeBookmark({bookmarkId: bookmarkId as string});
+    if (result.error && result.error.message.includes('You are offline!')) {
+      Alert.alert(
+        'You are offline',
+        'Please connect to the internet to remove this bookmark',
+      );
+    }
+  }, [removeBookmark, bookmarkId]);
   return (
     <Pressable
       onPress={() =>
@@ -58,12 +77,12 @@ export const Story: React.FC<StoryProps> = ({
           {title} {bookmarkId ? '🔖' : ''}
         </Text>
         {!bookmarkId && !isAddingBookmark && cta === 'add' && (
-          <Pressable onPress={() => addBookmark({storyId: id})}>
+          <Pressable onPress={handleAddBookmark}>
             <Text>Add Bookmark</Text>
           </Pressable>
         )}
         {bookmarkId && !isRemovingBookmark && cta === 'remove' && (
-          <Pressable onPress={() => removeBookmark({bookmarkId})}>
+          <Pressable onPress={handleRemoveBookmark}>
             <Text>Remove Bookmark</Text>
           </Pressable>
         )}
